d3dd1645748e700f24e44337776307569840d744
nthkmbm43 <nthkmbm43@gmail.com>
2026-04-02 14:21:52 +0700

    Update email_config.php

--- config/email_config.php
+++ config/email_config.php
@@ -5,12 +5,12 @@ define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 587);
 
 // Gmail ส่วนตัว
-define('SMTP_USERNAME', 'nthkmbm43@gmail.com');
+define('SMTP_USERNAME', 'YOUR_EMAIL');
 
 // รหัส App Password 16 หลัก (เขียนติดกัน)
 define('SMTP_PASSWORD', 'YOUR_APP_PASSWORD');
 
-define('MAIL_FROM', 'nthkmbm43@gmail.com');
+define('MAIL_FROM', 'YOUR_EMAIL');
 define('MAIL_FROM_NAME', 'ระบบข้อร้องเรียน (แจ้งเตือน)');
 
 define('SITE_NAME', 'ระบบข้อร้องเรียนนักศึกษา RMUTI');
